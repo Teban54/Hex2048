3dd2c7ec256efa69b52c9776d7b003cf81351c5a
Liang Lyu <ll289@duke.edu>
2017-11-02 20:31:48 -0400

    Hide status bar

--- app/src/main/java/teban54/hex2048/MainActivity.java
+++ app/src/main/java/teban54/hex2048/MainActivity.java
@@ -1,9 +1,13 @@
 package teban54.hex2048;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
