3bb7c461d906f512fa6e99465c937f62d6b6e544
Liang Lyu <ll289@duke.edu>
2017-11-02 20:19:36 -0400

    Getting started with Android Studio

---
+++ app/src/main/java/teban54/hex2048/MainActivity.java
@@ -0,0 +1,27 @@
+package teban54.hex2048;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    // Reference YouTube video: https://www.youtube.com/watch?v=-igAiudpBng
+    static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "Sample debug message");
+
+        Button sampleButton = (Button) (findViewById(R.id.sampleButton));
+        sampleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Clicked");
+            }
+        });
+    }
+}
